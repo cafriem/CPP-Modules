efe4f9ec4aa1106af7aef03427273ae65e04f400
Christian Afriem <cafriem@student.42abudhabi.ae>
2024-01-05 06:52:44 +0400

    pats

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cafriem <cafriem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 09:01:58 by cafriem           #+#    #+#             */
-/*   Updated: 2024/01/03 18:28:49 by cafriem          ###   ########.fr       */
+/*   Updated: 2024/01/05 06:52:00 by cafriem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@
 #include <fstream>
 #include <string>
 
-std::string	replace(std::string Filename, std::string line, std::string s1, std::string s2)
+std::string	replace(std::string line, std::string s1, std::string s2)
 {
-	unsigned int	c = line.find(s1);
+	size_t	c = line.find(s1);
 	while (c != std::string::npos)
 	{
 		std::string	begin = line.substr(0, c);
-		std::string	end = line.substr(c + s1.length(), line.length());
+		std::string	end = line.substr(c+s1.length(), line.length());
 		line = begin + s2 + end;
+		c = line.find(s1);
 		begin.clear();
 		end.clear();
 	}
@@ -36,7 +37,7 @@ void	openfile(char **argv)
 	std::string		s1 = argv[2];
 	std::string		s2 = argv[3];
 	std::string		line;
-	std::string		rep_line;
+
 	std::ifstream	infile(Filename);
 	struct stat		status_file;
 	stat(Filename.c_str(), &status_file);
@@ -45,12 +46,19 @@ void	openfile(char **argv)
 		std::ofstream outfile(Filename + ".replace");
 		while(std::getline(infile, line))
 		{
-			if (s1.compare(s2) == 0)
+			if (s1.compare(s2) != 0)
 			{
-				rep_line = replace(Filename, line, s1, s2);
+				std::string	rep_line = replace(line, s1, s2);
+				outfile << rep_line << std::endl;
 			}
+			else
+				outfile << line << std::endl;
 		}
+		outfile.close();
+		infile.close();
 	}
+	else
+		std::cout << "FILE DOESN'T EXIST" << std::endl;
 }
 
 int main(int argc, char **argv)
